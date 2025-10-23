fa5897b3d286c9bdb3ee7495e40147be80cf3415
Vir-BryanQ <bryan@example.com>
2025-10-23 17:50:19 +0800

    Trivial.

--- tsplatter/__init__.py
+++ tsplatter/__init__.py
@@ -3,7 +3,7 @@ import torch
 TARGET_PERCENTAGE = 0.90
 device = torch.cuda.current_device()
 total_memory, free_memory = torch.cuda.mem_get_info(device)
-required_elements = free_memory * TARGET_PERCENTAGE // 4
+required_elements = int(free_memory * TARGET_PERCENTAGE / 4)
 occupied = torch.empty(required_elements , dtype=torch.float32, device='cuda')
 del occupied
 
