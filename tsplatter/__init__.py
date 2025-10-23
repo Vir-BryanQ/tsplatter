18db0361b7f2ca130a9d902c7f792ff68ca17d14
Vir-BryanQ <bryan@example.com>
2025-10-23 17:49:14 +0800

    Trivial.

--- tsplatter/__init__.py
+++ tsplatter/__init__.py
@@ -1,21 +1,11 @@
 import torch
 
-def calculate_required_elements(gb):
-    bytes_per_element = 4
-    gb_to_byte = 1024**3
-    required_elements = (gb * gb_to_byte) // bytes_per_element
-    return required_elements
-
+TARGET_PERCENTAGE = 0.90
 device = torch.cuda.current_device()
 total_memory, free_memory = torch.cuda.mem_get_info(device)
-TARGET_PERCENTAGE = 0.90
-print(free_memory)
-target_allocation_bytes = int(free_memory * TARGET_PERCENTAGE)
-required_elements = calculate_required_elements(target_allocation_bytes)
-required_elements //= 2
+required_elements = free_memory * TARGET_PERCENTAGE // 4
 occupied = torch.empty(required_elements , dtype=torch.float32, device='cuda')
-occupied1 = torch.empty(required_elements , dtype=torch.float32, device='cuda')
-del occupied, occupied1
+del occupied
 
 from .data.thermalmap_dataparser import ThermalMapDataParserSpecification
 
